6bc1ccd0873742536fcc3428be2986a19c420ee8
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-02-26 03:04:52 +0530

    Set up client-side project with React, routing, and styling dependencies

--- Server/Routes/messages.routes.js
+++ Server/Routes/messages.routes.js
@@ -5,7 +5,7 @@ import { verifyToken } from "../Middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.get('/users', verifyToken, messageController.getUserSidebar);
-router.get('/:userId', verifyToken, messageController.getMessages);
+router.get('/:id', verifyToken, messageController.getMessages);
 
 router.post('/send/:id', verifyToken, messageController.sendMessages);
 
