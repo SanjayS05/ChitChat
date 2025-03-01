1c201fa0b7e0609f5e0b92ea7e7f2a97b2585269
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-03-01 23:05:47 +0530

    chore: Switch base URL to local development server

--- Client/src/Store/useAuthStore.js
+++ Client/src/Store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from '../Lib/axios.js'
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://chitchat-server-six.vercel.app/"
+const BASE_URL = "http://localhost:3000"
 export const useAuthStore = create((set, get) => ({
     authUser: null,
     isSigningUp: false,
